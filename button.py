b68841b3fc6f5a1c4342ebc3724ce57c043a650a
Hiler Chen <hilerchyn@gmail.com>
2014-04-19 00:43:43 +0800

    improve the button business logic

--- button.py
+++ button.py
@@ -3,6 +3,9 @@ import RPi.GPIO as GPIO
 button_pin_23 = 23
 button_pin_24 = 24
 
+pin_23_counter = 0
+pin_24_counter =0
+
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(button_pin_23, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -12,8 +15,17 @@ print ("button pin 23 and 24 (BCM GPIO)\n")
 
 while True:
         if (GPIO.input(button_pin_23)):
+            if(pin_23_counter==0):
+                pin_23_counter=1
                 print("pin 23 input")
         elif (GPIO.input(button_pin_24)):
+            if(pin_24_counter==0):
+                pin_24_counter=1
                 print("pin 24 input")
-        # else:
+        else:
+            if(pin_23_counter > 0):
+                pin_23_counter =0
+                
+            if(pin_24_counter > 0):
+                pin_24_counter =0
                 # print("no input")
