512d060cae1fd0b678ab36062c3593c5a77a4ebf
Hiler Chen <hilerchyn@gmail.com>
2014-04-19 01:14:32 +0800

    添加远程调用示例

--- button.py
+++ button.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+import sys, os, urllib2
 
 button_pin_23 = 23
 button_pin_24 = 24
@@ -17,15 +18,20 @@ while True:
         if (GPIO.input(button_pin_23)):
             if(pin_23_counter==0):
                 pin_23_counter=1
-                print("pin 23 input")
+                try:
+                    print urllib2.urlopen('http://192.168.0.106/test.php?arg1=xi&arg2=23').read()
+                except OSError, e:
+                    print "23 connection failed"
         elif (GPIO.input(button_pin_24)):
             if(pin_24_counter==0):
                 pin_24_counter=1
-                print("pin 24 input")
+                try:
+                    print urllib2.urlopen('http://192.168.0.106/test.php?arg1=xi&arg2=24').read()
+                except OSError, e:
+                    print "24 connection failed"
         else:
             if(pin_23_counter > 0):
                 pin_23_counter =0
                 
             if(pin_24_counter > 0):
                 pin_24_counter =0
-                # print("no input")
